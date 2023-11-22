82ea3ac13355fec664ddd9db3f2a75fdfda2c965
David Berard <dberard@fb.com>
2023-11-22 14:13:47 -0800

    [WIP] Do not specialize 0/1 eagerly for storage offset
    ghstack-source-id: 32e2893d5cd5b32591c915d2c1db380de6e0b8cd
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/114010

--- torch/fx/experimental/symbolic_shapes.py
+++ torch/fx/experimental/symbolic_shapes.py
@@ -2118,6 +2118,8 @@ class ShapeEnv:
             TensorPropertySource(source, TensorProperty.STORAGE_OFFSET),
             dynamic_dim=DimDynamic.DYNAMIC,
             constraint_dim=None,
+            do_not_specialize_zero_one=True,
+            positive=None,
         ), hint=ex_storage_offset, source=TensorPropertySource(source, TensorProperty.STORAGE_OFFSET))
         return tuple(sym_sizes), tuple(sym_stride), sym_storage_offset
 
