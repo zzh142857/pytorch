966ba5f9ea559626e1b667fd8e345f7cc1f2ab3d
voznesenskym <voznesenskym@gmail.com>
2024-01-18 15:36:59 -0800

    [dynamo][distributed] handle _rank_not_in_group, _get_or_create_default_group
    ghstack-source-id: 8c9d78448c56c813d100380673ac4506fb9276fe
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/117692
    
    Test

--- test/dynamo/test_distributed.py
+++ test/dynamo/test_distributed.py
@@ -1,12 +1,30 @@
 # Owner(s): ["module: dynamo"]
 
+import os
+import unittest
+
 import torch
 import torch._dynamo
 import torch._dynamo.test_case
 import torch._dynamo.testing
 
 from torch._dynamo.testing import normalize_gm
-import unittest
+
+
+def _get_device_type(world_size):
+    if torch.cuda.is_available() and torch.cuda.device_count() >= world_size:
+        device_type = "cuda"
+    else:
+        device_type = "cpu"
+    return device_type
+
+
+def _set_env_var(addr="localhost", port="25364", world_size=1, rank=0):
+    os.environ["MASTER_ADDR"] = addr
+    os.environ["MASTER_PORT"] = port
+    os.environ["WORLD_SIZE"] = f"{world_size}"
+    os.environ["RANK"] = f"{rank}"
+
 
 class DistributedTests(torch._dynamo.test_case.TestCase):
     @torch._dynamo.config.patch(trace_distributed=True)
@@ -56,6 +74,34 @@ class GraphModule(torch.nn.Module):
         return (_same_storage_size,)
 """,
             )
+
+    def test_process_group_get_or_create_default_group(self):
+        import torch.distributed.device_mesh as device_mesh
+
+        world_size = 1
+        rank = 0
+
+        device_type = _get_device_type(world_size)
+        _set_env_var(world_size=world_size, rank=rank)
+        mesh_tensor = torch.arange(1)
+
+        dmesh = device_mesh.DeviceMesh(device_type, mesh_tensor)
+        default = torch.distributed.distributed_c10d._get_default_group()
+
+        def foo(d, default, x):
+            group = d._get_or_create_default_group()
+            if group == default:
+                return x.cos()
+            return x.sin()
+
+        x = torch.randn([2, 2])
+        eager = foo(dmesh, default, x)
+        foo = torch._dynamo.optimize("eager", nopython=True)(foo)
+        comp = foo(dmesh, default, x)
+        foo = torch._dynamo.optimize("aot_eager", nopython=True)(foo)
+        comp_aot = foo(dmesh, default, x)
+        self.assertEqual(comp, eager)
+        self.assertEqual(comp_aot, comp)
 
 
 if __name__ == "__main__":
