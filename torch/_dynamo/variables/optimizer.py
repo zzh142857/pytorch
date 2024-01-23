00ef1721636d489f8c78ea561d9859ff94aec403
lezcano <lezcano-93@hotmail.com>
2024-01-23 09:52:06 +0000

    [dynamo,optim] Place guards on the args before assuming they exist
    This enables the new way of writing guards for dicts. Before we were
    doing things like
    ```
      L['self'].param_groups[0][___dict_keys_getitem(L['self'].param_groups[0], 0)][3] is L['self'].param_groups[0]['params'][3]
    ```
    without knowing whether `L['self'].param_groups[0][___dict_keys_getitem(L['self'].param_groups[0], 0)]` was a list.
    
    On a different note, I'll probably write a pass to recover the previous
    way to place guards on dicts via something like `DICT_KEYS`  as an
    optimisation, as it seems relevant for optimisers.
    
    ghstack-source-id: 9d3d9924ae77d2400e3cf271bfc4cbebe67018d0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/117983

--- torch/_dynamo/variables/optimizer.py
+++ torch/_dynamo/variables/optimizer.py
@@ -60,6 +60,14 @@ class OptimizerVariable(UserDefinedObjectVariable):
                 ret_val = self.value._init_group(*py_args, **py_kwargs)
                 self.map_sources_and_install_guards(tx)
                 self.update_list_args(tx, args, kwargs, py_args, py_kwargs)
+                # this next line has the side effect of installing guards
+                # It must be executed after update_list_args, as we first need to
+                # place the guards on the args / kwargs and then on the param_groups
+                from .builder import VariableBuilder
+
+                VariableBuilder(tx, AttrSource(self.source, "param_groups"))(
+                    self.value.param_groups
+                ).recursive_realize()
                 # stash a weak_ptr to optimizer to invalidate code
                 # if the optimizer object dies
                 tx.store_global_weakref(self.get_global_name(), self.value)
@@ -106,8 +114,6 @@ class OptimizerVariable(UserDefinedObjectVariable):
         return new_args, new_kwargs
 
     def map_sources_and_install_guards(self, tx):
-        from .builder import VariableBuilder
-
         self.grad_to_source = {}
         self.tensor_to_source = {}
 
@@ -149,11 +155,6 @@ class OptimizerVariable(UserDefinedObjectVariable):
                     )
                 else:
                     raise GuardInstallException()
-
-        # this next line has the side effect of installing guards
-        VariableBuilder(tx, AttrSource(self.source, "param_groups"))(
-            self.value.param_groups
-        ).recursive_realize()
 
     def wrap_tensor(self, tx, tensor_value):
         """Wrap state tensor in a TensorVariable"""
