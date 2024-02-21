b640ab9f07e64fb7055d7eb037d866bc622f0590
Brian Hirsh <hirsheybar@fb.com>
2024-02-21 15:08:45 -0800

    AOTAutograd: ensure traced tangent subclass metadata takes non-contiguous outputs into account
    ghstack-source-id: e9feeb4299db4f48dbec64004cf05d49316f4595
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/118669

--- torch/_functorch/_aot_autograd/input_output_analysis.py
+++ torch/_functorch/_aot_autograd/input_output_analysis.py
@@ -18,6 +18,7 @@ from torch import Tensor
 from torch._logging import getArtifactLogger
 from torch._subclasses.functional_tensor import FunctionalTensor
 from torch.fx.experimental.symbolic_shapes import is_concrete_int
+from .collect_metadata_analysis import coerce_tangent
 from .schemas import (
     BackwardSignature,
     GraphSignature,
@@ -47,6 +48,7 @@ def remove_dupe_metadata(
     other_traced_tangents = m.traced_tangents[num_data_mutations:]
     inp_traced_tangents = m.traced_tangents[:num_data_mutations]
     filtered_inp_traced_tangents = [
+        # See Note [Tangents must be contiguous]
         x
         for i, x in enumerate(inp_traced_tangents)
         if keep_arg_mask[m.mutated_inp_runtime_indices[i]]
@@ -226,7 +228,8 @@ def create_synthetic_base_metadata(
         )
 
     inner_mutated_tangents = [
-        x
+        # See Note [Tangents must be contiguous]
+        coerce_tangent(x)
         for inner_idx, x in enumerate(inner_args)
         if input_infos[inner_idx].mutates_data and input_infos[inner_idx].requires_grad
     ]
