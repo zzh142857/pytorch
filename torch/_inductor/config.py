44f0c82bb1d149c8b7f4ddd803126aa16a1ff313
Elias Ellison <elias.ellison@gmail.com>
2024-02-22 01:39:09 +0000

    test enable
    ghstack-source-id: 216733416282c66a8f3ba8a9020cf3f7cd37a6fc
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/120278

--- torch/_inductor/config.py
+++ torch/_inductor/config.py
@@ -275,7 +275,7 @@ enabled_metric_tables = os.environ.get("TORCHINDUCTOR_ENABLED_METRIC_TABLES", ""
 debug_filter_choice: Optional[Callable[[Any], bool]] = None
 
 benchmark_multi_templates = (
-    os.environ.get("TORCHINDUCTOR_BENCHMARK_MULTI_TEMPLATES", "0") == "1"
+    os.environ.get("TORCHINDUCTOR_BENCHMARK_MULTI_TEMPLATES", "1") == "1"
 )
 
 # how many nodes to allow into a single fusion
