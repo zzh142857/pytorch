c267ccefca9352f7a386e2ee9309ea8ccbeb95af
Catherine Lee <csl@fb.com>
2024-02-23 11:44:44 -0800

    appease lint

--- test/test_ci_sanity_check_fail.py
+++ test/test_ci_sanity_check_fail.py
@@ -2,14 +2,15 @@
 # Sanity check for CI setup in GHA.  This file is expected to fail so it can trigger reruns
 
 import os
+
 from torch.testing._internal.common_utils import run_tests, TestCase
 
 
 class TestCISanityCheck(TestCase):
     def test_env_vars_exist(self):
         # This check should fail and trigger reruns.  If it passes, something is wrong
-        self.assertTrue(os.environ.get("CI") == None)
+        self.assertTrue(os.environ.get("CI") is None)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_tests()
